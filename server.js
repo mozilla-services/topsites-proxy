dd8138f17c780c2d153c3c4d9fabb0b21e5bdf4d
Mike de Boer <info@mikedeboer.nl>
2020-09-09 11:44:37 +0200

    Fix tests that pass no user-agent string to the server.

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.use("/cid/:cid", (req, res) => {
     target,
     headers: {
       // We omit the platform data from the user-agent string.
-      "user-agent": req.headers["user-agent"].replace(/\(([^;]+);.*(rv:[\d.]+)\)/i, "($1; $2)")
+      "user-agent": (req.headers["user-agent"] || "").replace(/\(([^;]+);.*(rv:[\d.]+)\)/i, "($1; $2)")
     }
   });
 });
