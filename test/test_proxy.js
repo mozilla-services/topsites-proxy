bc7047e136a3a4aa621958c5be51701c98e1e9c9
Greg Guthe <gguthe@mozilla.com>
2020-09-09 17:44:52 -0400

    test: invalid cid to /cid/:cid

--- test/test_proxy.js
+++ test/test_proxy.js
@@ -3,6 +3,29 @@ const http = require("http");
 const { withServer, checkServerLogs, PORT, STOP } = require("./utils");
 
 describe("Top Sites proxy endpoint", function() {
+
+  it("should return 500 for request to /cid/:cid with an invalid cid", async function() {
+    return withServer(async server => {
+      const cid = "not_found";
+      const logsPromise = checkServerLogs(server, [`invalid campaign identifier: ${cid}`]);
+
+      let res = await new Promise(resolve => http.get(`http://localhost:${PORT}/cid/${cid}`, resolve));
+      Assert.equal(res.statusCode, 500);
+
+      let data = await new Promise(resolve => {
+        res.setEncoding("utf8");
+        let rawData = "";
+        res.on("data", chunk => rawData += chunk);
+        res.on("end", () => {
+          resolve(rawData);
+        });
+      });
+      await logsPromise;
+
+      Assert.ok(data);
+    });
+  });
+
   it("should handle proper requests to /cid/:cid properly", async function() {
     return withServer(async server => {
       const cid = "amzn_2020_1";
